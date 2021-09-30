ad22bfc568090a80516cd6ec9e987f31c08e6638
aaronhnash <77747939+aaronhnash@users.noreply.github.com>
2021-09-30 12:37:12 -0600

    Added commented code for RPi.GPIO

--- Server/RequestHandler.py
+++ Server/RequestHandler.py
@@ -1,6 +1,25 @@
-import socketserver, threading
+import socketserver, threading, time
 from typing import Type
-#import GPIO
+#import RPi.GPIO as GPIO
+
+#GPIO.setmode(BCM)
+# GPIO pin mapping & setup
+pinLED = 18
+pinServo = 17
+
+#GPIO.setup(pinLED, GPIO.OUT)
+#GPIO.setup(pinServo, GPIO.OUT)
+
+#servo = GPIO.PWM(pinServo, 50)
+#servo.start(0)
+
+# Some pre-defined duty cycles, will add as the session goes on to speed up caclulations.
+duty_cycle = {
+    0:[1.5, 7.5],
+    90:[2, 10],
+    -90:[1, 5] 
+}
+
 
 class RequestHandler(socketserver.BaseRequestHandler):
 
@@ -24,22 +43,30 @@ class RequestHandler(socketserver.BaseRequestHandler):
             response = bytes(f"Echoing '{command[1]}'", "ascii")
 
         elif command[0] == "blink":
-            try:
-                self.blink(int(command[1]))
-                n = command[1]
-            except: # If there's an error, then run as default.
-                self.blink()
+
+            if command[1] == None:
                 n = 1
+            else:
+                n = command[1]
             response = bytes(f"Blinking {n} time(s)", "ascii")
 
+            try:
+                self.blink(int(command[1]))
+            except: # If there's an error, then run as default.
+                self.blink()
+
         elif command[0] == "rotate":
+
+            if command[1] == None:
+                n = 0
+            else:
+                n = command[1]
+            response = bytes(f"Rotating to {n} degrees", "ascii")
+
             try:
                 self.rotate(int(command[1]))
-                n = command[1]
             except:
                 self.rotate()
-                n = 0
-            response = bytes(f"Rotating {n} degrees", "ascii")
 
         elif command[0] == "help":
             if command[1] == "echo":
@@ -47,7 +74,7 @@ class RequestHandler(socketserver.BaseRequestHandler):
             elif command[1] == "blink":
                 response = bytes("Usage: blink (integer); blinks LED n times. Default 1.", "ascii")
             elif command[1] == "rotate":
-                response = bytes("Usage: rotate (integer); rotates servo n degrees. Default 0, range (-90, 90).", "ascii")
+                response = bytes("Usage: rotate (integer); rotates servo to n degrees. Default 0, range (-90, 90).", "ascii")
             else:
                 response = bytes("Usage: help (command); available commands: echo, blink, rotate, help", "ascii")
 
@@ -62,12 +89,25 @@ class RequestHandler(socketserver.BaseRequestHandler):
     def blink(self, count=1):
         """Turns an external LED on and off n times. Default 1."""
         #TODO: Get GPIO mapping and see which pin the LED is attached to. 
-
-        pass
+        for n in range(count):
+            print(f"Blink {n+1}...")
+            # GPIO.output(pinLED, HIGH)
+            time.sleep(0.3)
+            #GPIO.output(pinLED, LOW)
+            time.sleep(0.3)
 
     def rotate(self, deg=0):
         """Rotates an attached servo to a specified angle. Min -90, max 90."""
-        #TODO: Get GPIO mapping of servo.
-        #TODO: Should the default action just spin for a little bit, and then go the other way?
+        #TODO: Uncomment when connected to the RPi
 
-        pass
+        if deg in duty_cycle:
+            # servo.ChangeDutyCycle(duty_cycle[deg][1])
+            pass
+        else:
+            # calculate, add to dictionary, and then set the angle.
+            ms = deg*(1/180) + 1.5 # formula for calculating the first part of the duty cycle
+            cycle = ms/20 # converting from ms to percentage
+            duty_cycle[deg] = [ms, cycle]
+            # servo.ChangeDutyCycle(duty_cycle[deg][1])
+
+
